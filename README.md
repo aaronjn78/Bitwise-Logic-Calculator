bbb7063786286570ee366cb769342a03ff64a6d1
A N <aaronjn78@yahoo.com>
2024-05-30 12:45:26 -0400

    Second Commit
    added request for second number and improved logic in requesting valid user inputs

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Bit logic calculator
 
 This app takes 2 binary numbers and outputs the bitwise AND, OR and XOR results. Will be pushing the incremental stages of the development process to Github in order to practice both the usage of command line git and also developing code in stages towards a final product as opposed to everything at once.
+
+Commit 1: 
+    - initial code that takes user input and verifies it is a binary number
+
+Commit 2: 
+    - accidentally deleted s.close() from previous commit. Adding code
+    = added request to user for second binary number
+    - moved invalid entry message to method that checks for valid binary number
+
+    
+    
--- src/App.java
+++ src/App.java
@@ -16,17 +16,26 @@ public class App {
         String XORresult;
         Scanner s = new Scanner(System.in);
 
-        System.out.println("Enter first binary number");
-        userInput = s.nextLine();
-        binary1 = userInput.split("");
-        
+        do {
+            System.out.println("Enter first binary number");
+            userInput = s.nextLine();
+            binary1 = userInput.split("");
+        } while (!BinaryNumberCheck(binary1));
 
-        if (BinaryNumberCheck(binary1)) {
-            System.out.println("valid number");
-            } else {
-        System.out.println("invalid number");
-        }
-       
+        // if (BinaryNumberCheck(binary1)) {
+        // System.out.println("valid number");
+        // } else {
+        // System.out.println("invalid number");
+        // }
+
+        do {
+            System.out.println("Enter second binary number");
+            userInput = s.nextLine();
+            binary2 = userInput.split("");
+        } while (!BinaryNumberCheck(binary2));
+
+        s.close();
+
     }
 
     public static Boolean BinaryNumberCheck(String[] input) {
@@ -34,18 +43,14 @@ public class App {
         boolean valid = false;
         for (String x : input) {
 
-            if ((x.equals("1")) || (x.equals("0")))
-            {      
+            if ((x.equals("1")) || (x.equals("0"))) {
                 valid = true;
             } else {
                 valid = false;
+                System.out.println("The value entered is not a valid binary number.");
                 return valid;
             }
         }
-
         return valid;
-        
     }
-
-
 }
